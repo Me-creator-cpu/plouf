5d25223233fcb3f3378669139bc2b127e693b705
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2026-07-08 16:12:47 +0200

    db_drop_tests

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ filenameFull = filename + '.sqlite3'
 def os_build_path(pathtobuild):
     os.makedirs(pathtobuild, exist_ok=True)
 
+#def db_conn_check(conn):
+#     try:
+#        conn.cursor()
+#        return True
+#     except Exception as ex:
+#        return False
+#    #myconn = sqlite3.connect('test.db')
+#    st.write(chk_conn(myconn))
+
 def db_create(db_fullpath):
     db = sqlite3.connect(db_fullpath)
     db.execute('CREATE TABLE IF NOT EXISTS TableName (id INTEGER PRIMARY KEY, quantity INTEGER)')
@@ -35,6 +44,8 @@ def db_init_data_test(db_fullpath):
     connexion = sqlite3.connect(db_fullpath)
     curseur = connexion.cursor()
     donnees = [("toto", 1000), ("tata", 750), ("titi", 500)]
+    DROP TABLE IF EXISTS scores;
+
     curseur.execute("""CREATE TABLE IF NOT EXISTS scores(
                     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
                     pseudo TEXT,
@@ -45,7 +56,7 @@ def db_init_data_test(db_fullpath):
             donnees)
     connexion.commit()
     connexion.close()
-                    
+
 def db_read_test(db_fullpath):
     connexion = sqlite3.connect(db_fullpath)
     #connexion = sqlite3.connect(":memory:") # BDD dans la RAM
@@ -56,7 +67,101 @@ def db_read_test(db_fullpath):
 
     df = pd.read_sql_query("SELECT * FROM scores", connexion)
     df
+
     connexion.close()
+
+def db_drop_tests(db_fullpath):
+    connexion = sqlite3.connect(db_fullpath)
+    curseur = connexion.cursor()
+    curseur.executescript("""DROP TABLE IF EXISTS TableName;
+                          DROP TABLE IF EXISTS scores;""")
+    connexion.commit()
+    connexion.close()
+
+#==================================================================================================
+def db_connection(db_fullpath):
+    return sqlite3.connect(db_fullpath)
+
+def db_connection_close(conn, bCommit = False):
+    if bCommit:
+        conn.commit()
+    conn.close()        
+
+def db_commit(conn):
+    conn.commit()
+
+def db_table_to_df(tablename,conn,bShowTab=False):
+    df = pd.read_sql_query("SELECT * FROM " + tablename, conn)
+    if bShowTab:
+        df
+    return df
+
+def db_init_data(db_fullpath):
+    bAddParent = True
+    bAddEnfant = False
+    bAddResa = False
+
+    connexion = sqlite3.connect(db_fullpath)
+    curseur = connexion.cursor()
+
+    if bAddParent:
+                #  (parent_name, parent_tel, parent_mail)
+        parents = [("arnaud", "0102030405","test1@mail.com"), 
+                ("tata", "0607080910","test2@mail.com"), 
+                ("titi", "0103050700","test3@mail.com")]
+        curseur.execute("""CREATE TABLE IF NOT EXISTS t_parent(
+                        parent_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                        parent_name TEXT,
+                        parent_tel TEXT,
+                        parent_mail TEXT
+                        )""")
+        curseur.executemany(
+                "INSERT INTO t_parent (parent_name, parent_tel, parent_mail) VALUES (?, ?, ?)",
+                parents)
+        connexion.commit()
+        db_table_to_df("t_parent",connexion,True)
+        
+    if bAddEnfant:
+                #  (enfant_name, parent_id, enfant_niveau, enfant_annee)
+        enfants = [("eva", 0, 6, 2016), 
+                ("jean", 0, 12, 2010), 
+                ("paulo", 0, 12, 1994)]
+        curseur.execute("""CREATE TABLE IF NOT EXISTS t_enfant(
+                        enfant_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                        enfant_name TEXT,
+                        parent_id INTEGER,
+                        enfant_niveau INTEGER,
+                        enfant_annee INTEGER
+                        )""")
+        curseur.executemany(
+                "INSERT INTO t_enfant (enfant_name, parent_id, enfant_niveau, enfant_annee) VALUES (?, ?, ?, ?)",
+                enfants)
+        connexion.commit()
+        db_table_to_df("t_enfant",connexion,True)
+
+    if bAddResa:
+                       #(enfant_id, enfant_niveau, resa_date)
+        reservations = [(),
+                        (),
+                        (),
+                        (),
+                        ()]
+        curseur.execute("""CREATE TABLE IF NOT EXISTS t_reservation(
+                        resa_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                        enfant_id INTEGER,
+                        enfant_niveau INTEGER,
+                        resa_date TEXT
+                        )""")
+        curseur.executemany(
+                "INSERT INTO t_reservation (enfant_id, enfant_niveau, resa_date) VALUES (?, ?, ?)",
+                parents)
+        connexion.commit()
+        db_table_to_df("t_reservation",connexion,True)
+
+    connexion.close()
+
+
+#==================================================================================================
 
 def init_buttons():
     global data_path,filename,filenameFull
@@ -76,5 +181,9 @@ def init_buttons():
     if st.button('Get DB Data test'):
         st.write('Read DB...')
         db_read_test(db)
+
+    if st.button("Init data"):
+        db_drop_tests(db)
+        db_init_data(db)
 
 init_buttons()        
