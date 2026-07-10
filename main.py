9b2917196fe0fc8283ea588ed1f2b8ae432ec323
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2026-07-10 12:30:14 +0200

    Test

--- main.py
+++ main.py
@@ -260,9 +260,11 @@ def show_diff(
         st.write(rows, cols)
         for r in range(rows):
             st.write(f'{key_field} = {target_base['index'][r]}')
-            for c in range(cols):
+            #for c in range(cols):
+            #    st.write(f'updated field={c}')
+            #    st.write(f'updated value=TODO')
+            for c in target_base:
                 st.write(f'updated field={c}')
-                st.write(f'updated value=TODO')
 
     st.subheader("Lignes créées")
     inserted = pd.DataFrame(editor_key.get("added_rows"))
