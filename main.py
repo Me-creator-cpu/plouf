1fb185e18cc5e1cf4d2e5927cd85759876e7cdf9
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2026-07-10 10:55:28 +0200

    show_diff

--- main.py
+++ main.py
@@ -199,6 +199,59 @@ def init_buttons():
 #==================================================================================================
 # Fonctions données
 #==================================================================================================
+def highlight_changes(val):
+    color = f"color: black;" if val else "color:lightgray;"
+    background = f"background-color:lightgray;" if val else ""
+    return f"{color} {background}"
+
+def show_diff(
+    source_df: pd.DataFrame, modified_df: pd.DataFrame, editor_key: dict
+) -> None:
+    target = pd.DataFrame(editor_key.get("edited_rows")).transpose().reset_index()
+    modified_columns = [i for i in target.notna().columns if i != "index"]
+    source = source_df.iloc[target.index].reset_index()
+    target = target[modified_columns].reset_index()
+
+    changes = pd.merge(
+        source[modified_columns].reset_index(),
+        target,
+        how="outer",
+        on="index",
+        suffixes=["_BEFORE", "_AFTER"],
+    )
+    after_columns = [i for i in changes.columns if "_AFTER" in i]
+    for cl in changes:
+        if cl in after_columns:
+            new_col = cl.replace("_AFTER", "_BEFORE")
+            changes[cl] = changes[cl].fillna(changes[new_col])
+
+    st.subheader("Donées modifiées")
+    st.caption("Colonnes modifiées uniquement")
+
+    change_markers = changes.copy()
+    for cl in change_markers:
+        if cl in after_columns:
+            new_col = cl.replace("_AFTER", "_BEFORE")
+            change_markers[cl] = change_markers[cl] != change_markers[new_col]
+            change_markers[new_col] = change_markers[cl]
+    st.dataframe(
+        changes.style.apply(
+            lambda _: change_markers.applymap(highlight_changes), axis=None
+        ),
+        use_container_width=True,
+        hide_index=True,
+    )
+
+    st.subheader("Lignes crées")
+    inserted = pd.DataFrame(editor_key.get("added_rows"))
+    st.dataframe(inserted, use_container_width=True)
+    st.subheader("Lignes supprimées")
+    deleted = pd.DataFrame(editor_key.get("deleted_rows"))
+    st.dataframe(deleted, use_container_width=True)
+
+
+
+
 def db_parents_get(ID_Parent = None):
     global db
     connexion=db_connection(db)  
@@ -236,6 +289,7 @@ def pg_parent_adm():
     connexion=db_connection(db)  
     df = db_table_to_df("t_parent",connexion,False)
     db_connection_close(connexion)
+    st.subheader("Liste des parents ⬇️")
     editor_df = st.data_editor(
         df, 
         key="parent_edit", 
@@ -244,6 +298,9 @@ def pg_parent_adm():
         disabled=["parent_id"],
         hide_index=True,
     )
+
+    show_diff(source_df=df, modified_df=editor_df, editor_key=st.session_state["parent_edit"])
+    return True
     df_updated=st.session_state["parent_edit"]
     st.write(df_updated) 
 
