2123daef723b2f6f3085b604224c30dc9f39f53d
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2026-07-10 10:35:16 +0200

    Test

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ def pg_parent_adm():
     st.write(df_edited)
     if st.button('Mettre à jour'):
         for u in df_edited:
-            st.write(df[u])
+            st.write(df.loc[u])
         #db_parents_update(id,tel,email)
     st.divider()
     df_added   = df_updated.get("added_rows")
