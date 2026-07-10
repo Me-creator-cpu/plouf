f848d47d02fc5566aafe370b9117cc47b785c6dc
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2026-07-10 13:02:54 +0200

    Test

--- main.py
+++ main.py
@@ -272,9 +272,9 @@ def show_diff(
                     if target_base[c][r] != '#####':
                         st.write(f'updated field={c}')
                         st.write(f'New {c} = {target_base[c][r]}')
-            #sql = f'UPDATE t_parent SET parent_name){name} parent_email={email}, parent_tel={tel}  WHERE parent_id={id}'
-        if id != 0:
-            st.write(sql)
+                        sql = f'UPDATE t_parent SET {c}={target_base[c][r]} WHERE {key_field} = {target_base['index'][r]}'
+                    if id != 0:
+                        st.write(sql)
     st.subheader("Lignes créées")
     inserted = pd.DataFrame(editor_key.get("added_rows"))
     st.dataframe(inserted, width='stretch')
