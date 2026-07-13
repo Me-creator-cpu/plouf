29cdf280881df64b0b84e989929f2e40bec4e992
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2026-07-13 15:45:44 +0200

    Test

--- main.py
+++ main.py
@@ -550,7 +550,7 @@ def pg_enfant_adm():
     subtitle("Liste des enfants ⬇️")
     df_parent
     dd=dict(df_parent.T)
-
+    dd
     #mode = st.selectbox("Calendar Mode:", options=list(calendar_display.keys()), format_func=lambda x:calendar_display[ x ])
     #sel_parent = st.selectbox("Parent:", options=list(lst_parents.keys()), format_func=lambda x:lst_parents[ x ])
     sel_parent = st.selectbox("Parent:", options=list(dd), format_func=lambda x:dd[ x ])
