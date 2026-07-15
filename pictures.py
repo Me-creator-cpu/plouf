ce229ba421b06e889466ff39a5d2f155f9b1c8d3
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2026-07-15 18:07:17 +0200

    Test

--- pictures.py
+++ pictures.py
@@ -8,7 +8,8 @@ def pic(pic_url=None,pic_caption=None,pic_width='content'):
 def pic_list():
     for x in os.listdir('.//images'):
         if x.endswith(".jpg") or x.endswith(".png"):
-            st.badge(x, icon=":material/check:", color="green")
-            st.image('./images/'+x, caption=x)
+            #st.badge(x, icon=":material/check:", color="green")
+            pic('./images/'+x, pic_caption=x)
+            #st.image('./images/'+x, caption=x)
         else:
             st.text(x)
